46b5bb0c23696a8b9ce3d0bab9bf6012a77a30a8
Johan Larsson <johan_gavle1@hotmail.com>
2013-12-08 13:01:50 +0100

    Manual :)

--- MongoBox/MongoTests.cs
+++ MongoBox/MongoTests.cs
@@ -92,23 +92,6 @@ namespace MongoBox
                 Console.WriteLine(testClass.Value);
             }
         }
-
-        [Test]
-        public void NamedAndUnnamedTest()
-        {
-            Assert.AreEqual("Only value1 was supplied", DummyMethod(value1: 1));
-            Assert.AreEqual("Only value1 was supplied", DummyMethod(1));
-            Assert.AreEqual("Only value2 was supplied", DummyMethod(value2: 1));
-            Assert.AreEqual("Both arguments were supplied", DummyMethod(1, 2));
-        }
-        private string DummyMethod(int value1 = 0, int value2 = 0)
-        {
-            if (value1 != 0 && value2 != 0)
-                return "Both arguments were supplied";
-            if (value1 == 0)
-                return "Only value2 was supplied";
-            return "Only value1 was supplied";
-        }
     }
 
     public class TestClass
---
+++ MongoBox/Readme.txt
@@ -0,0 +1,5 @@
+﻿1) Download from here: http://www.mongodb.org/downloads
+2) Unzip to C:\Mongo
+3) Create C:\data\db
+4) Run Mongod.exe (The console thing must stay open)
+5) Then the tests should work
